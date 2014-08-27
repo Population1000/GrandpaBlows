7c2cb331650fe627d8a876217137ff734b1f3cb2
mjjohns <kaleidoscapester@gmail.com>
2014-08-27 16:15:46 -0500

    Made test scene

---
+++ GrandpaBlows/Assets/Scripts/HeadRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Input.gyro.attitude;
+	
+	}
+}
