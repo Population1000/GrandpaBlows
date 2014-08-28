c9d6845fb2438dfad58bd289cc4731ae3c2846fc
BagelBob <rstephens917@gmail.com>
2014-08-28 16:04:33 -0500

    Temp change
    WHO KNOWS?

--- GrandpaBlows/Assets/Scripts/reticleScript.cs
+++ GrandpaBlows/Assets/Scripts/reticleScript.cs
@@ -4,50 +4,50 @@ using System.Collections;
 public class reticleScript : MonoBehaviour {
 	
 	public GameObject grandpa;
-	public GameObject blow;
-	public Vector3 scaleRate = new Vector3(0.01f,0.0f,0.01f);
+	public Vector3 inhaleRate = new Vector3(0.0f,0.01f,0.01f);
+	public Vector3 exhaleRate = new Vector3(0.0f,0.02f,0.02f);
+	public float breathLength = 0.85f;
 	public bool mouseDown = false;
 	public bool blowing = false;
 	// Use this for initialization
 	void Start () {
 		grandpa = GameObject.FindGameObjectWithTag ("MainCamera");
-		blow = GameObject.FindGameObjectWithTag ("Blow");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = grandpa.transform.rotation;
-		transform.Rotate (Vector3.right * 270);
-		transform.position = grandpa.transform.position;
-		transform.Translate (Vector3.down * 1);
-		blow.transform.rotation = transform.rotation;
-		blow.transform.position = transform.position;
-		if (Input.GetMouseButtonDown (0))
+//		transform.rotation = grandpa.transform.rotation;
+//		transform.Rotate (Vector3.right * 270);
+//		transform.position = grandpa.transform.position;
+//		transform.Translate (Vector3.down * 1);
+		if (Input.GetMouseButtonDown (0)) {
 			mouseDown = true;
+			blowing = false;
+		}
 		if (Input.GetMouseButtonUp (0)) {
 			blowing = true;
 			mouseDown = false;
 		}
 		if (mouseDown)
-			transform.localScale = transform.localScale + scaleRate;
+			transform.localScale = transform.localScale + inhaleRate;
 		else
-			transform.localScale = transform.localScale - scaleRate;
-		if (transform.localScale.x < .01f) {
+			transform.localScale = transform.localScale - exhaleRate;
+		if (transform.localScale.y < .01f) {
 			blowing = false;
-			transform.localScale = new Vector3 (0.01f, 0.00000001f, 0.01f);
+			transform.localScale = new Vector3 (breathLength, 0.011f, 0.011f);
 		}
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.CompareTag("Flame"))
-			if(blowing)
-				Destroy(other.gameObject);
+		if (other.CompareTag ("Flame"))
+			if (blowing)
+				other.gameObject.setActive(false);
 	}
 	void OnTriggerStay(Collider other)
 	{
 		if(other.CompareTag("Flame"))
 			if(blowing)
-				Destroy(other.gameObject);
+				other.gameObject.setActive(false);
 	}
 }
