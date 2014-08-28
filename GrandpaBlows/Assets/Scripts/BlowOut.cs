d8bb33c4a816b1e450f849d004f65861c11ca0e8
BagelBob <rstephens917@gmail.com>
2014-08-28 15:58:39 -0500

    Merge remote-tracking branch 'origin/master' into experimental
    Conflicts:
    	GrandpaBlows/Assets/Scenes/LevelPrototype.unity
    	GrandpaBlows/Assets/Scripts/reticleScript.cs
    	GrandpaBlows/Assets/Scripts/reticleScript.cs.meta
    	GrandpaBlows/GrandpaBlows-csharp.sln
    	GrandpaBlows/GrandpaBlows.sln
    	GrandpaBlows/GrandpaBlows.userprefs
    	GrandpaBlows/Library/CurrentLayout.dwlt
    	GrandpaBlows/Library/InspectorExpandedItems.asset
    	GrandpaBlows/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	GrandpaBlows/Library/ScriptAssemblies/Assembly-UnityScript.dll.mdb
    	GrandpaBlows/Library/assetDatabase3
    	GrandpaBlows/Library/expandedItems
    	GrandpaBlows/Library/guidmapper
    	GrandpaBlows/ProjectSettings/TagManager.asset

---
+++ GrandpaBlows/Assets/Scripts/BlowOut.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlowOut : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c){
+		if (c.tag == "Flame") {
+			foreach(Transform child in c.transform){
+				if(child.name == "Flame")
+					child.gameObject.SetActive(false);
+				else
+					child.gameObject.SetActive(true);
+			}
+				}
+	}
+}
