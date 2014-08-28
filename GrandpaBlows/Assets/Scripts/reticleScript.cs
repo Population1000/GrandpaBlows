38f3056a3a2153d7e4cb777d133951435d38332c
BagelBob <rstephens917@gmail.com>
2014-08-28 14:20:50 -0500

    Reticle Script changes
    Also removed MJ's script from the breath cone

--- GrandpaBlows/Assets/Scripts/reticleScript.cs
+++ GrandpaBlows/Assets/Scripts/reticleScript.cs
@@ -4,26 +4,23 @@ using System.Collections;
 public class reticleScript : MonoBehaviour {
 	
 	public GameObject grandpa;
-	public GameObject blow;
-	public Vector3 scaleRate = new Vector3(0.01f,0.0f,0.01f);
+	public Vector3 scaleRate = new Vector3(0.0f,0.01f,0.01f);
 	public bool mouseDown = false;
 	public bool blowing = false;
 	// Use this for initialization
 	void Start () {
 		grandpa = GameObject.FindGameObjectWithTag ("MainCamera");
-		blow = GameObject.FindGameObjectWithTag ("Blow");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = grandpa.transform.rotation;
-		transform.Rotate (Vector3.right * 270);
-		transform.position = grandpa.transform.position;
-		transform.Translate (Vector3.down * 1);
-		blow.transform.rotation = transform.rotation;
-		blow.transform.position = transform.position;
+//		transform.rotation = grandpa.transform.rotation;
+//		transform.Rotate (Vector3.right * 270);
+//		transform.position = grandpa.transform.position;
+//		transform.Translate (Vector3.down * 1);
 		if (Input.GetMouseButtonDown (0))
 			mouseDown = true;
+			blowing = false;
 		if (Input.GetMouseButtonUp (0)) {
 			blowing = true;
 			mouseDown = false;
@@ -32,9 +29,9 @@ public class reticleScript : MonoBehaviour {
 			transform.localScale = transform.localScale + scaleRate;
 		else
 			transform.localScale = transform.localScale - scaleRate;
-		if (transform.localScale.x < .01f) {
+		if (transform.localScale.y < .01f) {
 			blowing = false;
-			transform.localScale = new Vector3 (0.01f, 0.00000001f, 0.01f);
+			transform.localScale = new Vector3 (0.85f, 0.011f, 0.011f);
 		}
 	}
 
